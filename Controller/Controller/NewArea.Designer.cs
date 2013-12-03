192c1506829a741fbac6adabd8c3afd08875fe89
tonghuashuai <tonghuashuai@126.com>
2013-12-03 12:08:02 +0800

    new area form

---
+++ Controller/Controller/NewArea.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Controller
+{
+    partial class NewArea
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtArea
+            // 
+            this.txtArea.Location = new System.Drawing.Point(52, 29);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.Size = new System.Drawing.Size(129, 21);
+            this.txtArea.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(205, 28);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "确定";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(16, 33);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(29, 12);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "名称";
+            // 
+            // NewArea
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 78);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtArea);
+            this.MaximizeBox = false;
+            this.Name = "NewArea";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "新建区域";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtArea;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblName;
+    }
+}
---
+++ Controller/Controller/NewArea.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace Controller
+{
+    public partial class NewArea : Form
+    {
+        public NewArea()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string areaName = this.txtArea.Text;
+            if (string.IsNullOrEmpty(areaName))
+            {
+                MessageBox.Show("区域名不可为空！");
+            }
+            else
+            {
+                this.CreateAreaFile(areaName);
+            }
+        }
+
+        public void CreateAreaFile(string areaName)
+        {
+            try
+            {                
+                string xmlFilePath = areaName + ".xml";
+                FileInfo file = new FileInfo(xmlFilePath);
+                if (file.Exists)
+                {
+                    MessageBox.Show("改区域已存在！");
+                }
+                else
+                {
+                    XmlDocument myXmlDoc = new XmlDocument();
+                    //创建xml的根节点
+                    XmlElement rootElement = myXmlDoc.CreateElement("root");
+                    //将根节点加入到xml文件中（AppendChild）
+                    myXmlDoc.AppendChild(rootElement);
+                    myXmlDoc.Save(xmlFilePath);
+                    MessageBox.Show("区域添加成功！");
+                    this.Close();
+                }                
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("创建区域失败！");
+            }
+        }
+    }
+}
